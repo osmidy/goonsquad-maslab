8a3ed333654050394c7fc983da76dd5d6a40b40d
Olivier Midy <osmidy@mit.edu>
2015-01-12 18:06:23 -0500

    Fixed compilation errors

--- src/sensorIO_Gpio.cpp
+++ src/sensorIO_Gpio.cpp
@@ -1,13 +1,14 @@
 #include <jni.h>
 #include <stdio.h>
+#include <cstring>
 #include "mraa.hpp"
 #include "sensorIO_Gpio.h"
 
 JNIEXPORT jlong JNICALL Java_sensorIO_Gpio_init
   (JNIEnv *env, jobject thisObj, jint pin, jstring direction) {
-	mraa::Gpio gpio = new mraa::Gpio(pin);
+	mraa::Gpio gpio = new mraa::Gpio((int)pin);
 	jlong pointer = (jlong)&gpio;
-	const char *dir = (*env)->GetStringUTFChars(env, direction, NULL);
+	const char *dir = env->GetStringUTFChars(env, direction, NULL);
 	if (strcmp (dir, "out") == 0) {
 		gpio.dir(DIR_OUT);
 	}
@@ -20,13 +21,13 @@ JNIEXPORT jlong JNICALL Java_sensorIO_Gpio_init
 
 JNIEXPORT jint JNICALL Java_sensorIO_Gpio_read
   (JNIEnv *env, jobject thisObj, jlong pointer) {
-	mraa::Gpio gpio = (mraa::Gpio)*pointer;
+	mraa::Gpio gpio = (mraa::Gpio)((long)*pointer);
 	jint reading = (jint)gpio.read();
 	return reading;
 }
 
 JNIEXPORT void JNICALL Java_sensorIO_Gpio_write
   (JNIEnv *env, jobject thisObj, jlong pointer, jint value) {
-	mraa::Gpio gpio = (mraa::Gpio)*pointer;
+	mraa::Gpio gpio = (mraa::Gpio)((long)*pointer);
 	gpio.write((int)value);
 }
