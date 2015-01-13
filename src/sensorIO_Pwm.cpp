520fe9bc1157fb41dbb15da9516cad6914604c21
Olivier Midy <osmidy@mit.edu>
2015-01-13 13:25:44 -0500

    Fixed Pwm C++

--- src/sensorIO_Pwm.cpp
+++ src/sensorIO_Pwm.cpp
@@ -5,31 +5,31 @@
 
 JNIEXPORT jlong JNICALL Java_sensorIO_Pwm_init
   (JNIEnv *env, jobject thisObj, jint pin) {
-	mraa::Pwm pwm = new mraa::Pwm((int)pin);
-	pwm.write(0.0);
-	pwm.enable(true);
-	jlong pointer = (jlong)&pwm;
+	mraa::Pwm* pwm = new mraa::Pwm((int)pin);
+	pwm->write(0.0);
+	pwm->enable(true);
+	jlong pointer = (jlong)pwm;
 	return pointer;
 }
 
 JNIEXPORT void JNICALL Java_sensorIO_Pwm_setSpeed
   (JNIEnv *env, jobject thisObj, jlong pointer, jdouble speed) {
-	mraa::Pwm pwm = (mraa::Pwm)*pointer;
+	mraa::Pwm* pwm = (mraa::Pwm*)pointer;
 	float absSpeed = fabs((double)speed);
-	pwm.write(absSpeed);
+	pwm->write(absSpeed);
 }
 
 JNIEXPORT jdouble JNICALL Java_sensorIO_Pwm_getSpeed
   (JNIEnv *env, jobject pointer, jlong pointer) {
-	mraa::Pwm pwm = (mraa::Pwm)*pointer;
-	jdouble speed = (jdouble)pwm.read();
+	mraa::Pwm* pwm = (mraa::Pwm*)pointer;
+	jdouble speed = (jdouble)pwm->read();
 	return speed;
 }
 
 JNIEXPORT void JNICALL Java_sensorIO_Pwm_setEnableStatus
   (JNIEnv *env, jobject thisObj, jlong pointer, jboolean enable) {
-	mraa::Pwm pwm = (mraa::Pwm)*pointer;
+	mraa::Pwm* pwm = (mraa::Pwm*)pointer;
 	bool boolean = (bool)enable;
-	pwm.enable(boolean);
+	pwm->enable(boolean);
 }
 
