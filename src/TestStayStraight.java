b779a405e5c07936113ee27d552f005663dd7668
Olivier Midy <osmidy@mit.edu>
2015-01-16 20:13:53 -0500

    Changing PID things

--- src/TestStayStraight.java
+++ src/TestStayStraight.java
@@ -62,17 +62,17 @@ public class TestStayStraight {
             double omega = gyro.getAngularVelocity(gyro.getChipPointer(), gyro.getSpiPointer());
             if (heading <= -0.5) {
                 double leftSpeed = leftMotor.getSpeed();
-                leftSpeed += .0001 * heading;
+                leftSpeed += .001 * heading;
                 double rightSpeed = rightMotor.getSpeed();
-                rightSpeed -= .0001 * heading;
+                rightSpeed -= .001 * heading;
                 leftMotor.setSpeed(leftSpeed);
                 rightMotor.setSpeed(rightSpeed);
             }
             if (heading >= 0.5) {
                 double rightSpeed = rightMotor.getSpeed();
-                rightSpeed += .0001 * heading;
+                rightSpeed += .001 * heading;
                 double leftSpeed = leftMotor.getSpeed();
-                leftSpeed -= .0001 * heading;
+                leftSpeed -= .001 * heading;
                 rightMotor.setSpeed(rightSpeed);
                 leftMotor.setSpeed(leftSpeed);
             }
