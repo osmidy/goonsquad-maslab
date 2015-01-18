6a53317a84060d9d7739f9ba8ef90dca3b444043
George Liang <gcliang@mit.edu>
2015-01-17 19:34:00 -0500

    Control Testing

--- src/TestStayStraight.java
+++ src/TestStayStraight.java
@@ -104,7 +104,7 @@ public class TestStayStraight {
             }
             
             long fin = System.currentTimeMillis();
-            if ((fin - current) >= 5000) {
+            if ((fin - current) >= 10000) {
                 leftMotor.setSpeed(0);
                 rightMotor.setSpeed(0);
                 break outerloop;
