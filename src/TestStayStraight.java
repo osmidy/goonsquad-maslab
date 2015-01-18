c8723b621400cdcf1553715e412762a795936ea0
George Liang <gcliang@mit.edu>
2015-01-17 20:51:50 -0500

    Control Testing

--- src/TestStayStraight.java
+++ src/TestStayStraight.java
@@ -55,7 +55,7 @@ public class TestStayStraight {
         getHeading.start();
         long current = System.currentTimeMillis();
         double motorBias = 0.2;
-        double p = .015;
+        double p = .012;
         double i = .0005;
         double d = .03;
         long begin = System.currentTimeMillis();
