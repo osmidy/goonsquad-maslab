86cdbf1b659b76fa25f4e055b2b05e2403c2e46b
George Liang <gcliang@mit.edu>
2015-01-19 16:34:45 -0500

    WallFollow Update

--- src/TestWallFollowing.java
+++ src/TestWallFollowing.java
@@ -33,7 +33,7 @@ public class TestWallFollowing {
         long current = System.currentTimeMillis();
         long begin = System.currentTimeMillis();
         double bias = 0;
-        double p = 0; // .012;
+        double p = 0.02; // .012;
         double i = 0; // .0005;
         double d = 5; // .03;
         double integral = 0;
