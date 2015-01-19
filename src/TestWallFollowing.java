ed233ae28f114f1a81d38d5b2f53caab475a3865
George Liang <gcliang@mit.edu>
2015-01-19 16:29:44 -0500

    WallFollow Update

--- src/TestWallFollowing.java
+++ src/TestWallFollowing.java
@@ -6,11 +6,6 @@ import sensorIO.Pwm;
 
 public class TestWallFollowing {
     public static void main(String[] args) {
-        String DIR_OUT = "out";
-        long pwmPointerLeft;
-        long dirPointerLeft;
-        long pwmPointerRight;
-        long dirPointerRight;
 
         int leftForward = 1;
         int leftReverse = 0;
@@ -27,12 +22,10 @@ public class TestWallFollowing {
 
         Motor leftMotor = new Motor(pwmPinLeft, dirPinLeft, leftForward,
                 leftReverse);
-        pwmPointerLeft = leftMotor.getPwmPin();
-        dirPointerLeft = leftMotor.getGpioPin();
+
         Motor rightMotor = new Motor(pwmPinRight, dirPinRight, rightForward,
                 rightReverse);
-        pwmPointerRight = rightMotor.getPwmPin();
-        dirPointerRight = rightMotor.getGpioPin();
+
         IRSensor forwardSensor = new IRSensor(forwardPin);
         IRSensor rearSensor = new IRSensor(rearPin);
         
@@ -42,14 +35,13 @@ public class TestWallFollowing {
         double bias = 0;
         double p = 0; // .012;
         double i = 0; // .0005;
-        double d = 50; // .03;
+        double d = 5; // .03;
         double integral = 0;
         double derivative = 0;
         double separation = .1;
         double frontSep = forwardSensor.distanceToObject();
         double rearSep = rearSensor.distanceToObject();  
-        double diff = frontSep -rearSep;
-        System.out.println("Front: " + frontSep + "Rear: " + rearSep);
+        double diff = frontSep - rearSep;
         double prevDiff = 0;
         leftMotor.setSpeed(bias);
         rightMotor.setSpeed(bias);
@@ -63,6 +55,9 @@ public class TestWallFollowing {
             double deltaT = .001 * (end - begin);
             integral += diff * deltaT;
             derivative = diff - prevDiff;
+            if (integral > 500) {
+                integral = 500;
+            }
             double power = p * diff + i * integral + d * derivative;
             leftMotor.setSpeed(bias - power);
             rightMotor.setSpeed(bias + power);
