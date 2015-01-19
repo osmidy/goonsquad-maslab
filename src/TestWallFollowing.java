b786e5faf921747cc6f7af11cc7d0c9c2b7e3816
George Liang <gcliang@mit.edu>
2015-01-19 18:37:54 -0500

    WallFollow Update

--- src/TestWallFollowing.java
+++ src/TestWallFollowing.java
@@ -35,7 +35,7 @@ public class TestWallFollowing {
         double bias = 0;
         double p = 3.5; // 3.5; // .012;
         double i = 0; // 1; // .0005;
-        double d = 0; // 0.1; // .03;
+        double d = 3; // 0.1; // .03;
         double integral = 0;
         double derivative = 0;
         double separation = .1;
