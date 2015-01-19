895a39597b04d2cdd260492dd33bd38c94be2b1b
Chicken <medinaa@mit.edu>
2015-01-19 13:38:54 -0500

    just pulling stuff

--- src/TestRunMotor.java
+++ src/TestRunMotor.java
@@ -35,7 +35,7 @@ public class TestRunMotor {
         pwm.setSpeed(pwmPointer, 0.15);
         pwmRight.setSpeed(pwmPointerRight, 0.15);
         dirRight.write(dirPointerRight, 0);
-        
+        /**
         //make gyro
         final Gyroscope gyro = new Gyroscope(10);
         long chip = gyro.getChipPointer();
@@ -49,14 +49,15 @@ public class TestRunMotor {
         	pwm.setSpeed(pwmPointer, 0.15-0.03*power);
         	pwmRight.setSpeed(pwmPointerRight, 0.15+0.03*power);
         }
+        */
         // Stop after 5 seconds
-//        try {
-//            Thread.sleep(5000);
-//        } catch (InterruptedException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//        
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
         pwm.setSpeed(pwmPointer, 0.0);
         pwmRight.setSpeed(pwmPointerRight, 0.0);
     }
