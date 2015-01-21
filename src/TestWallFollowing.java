54090a9d768e216675d03257bcbfecca3335d5af
George Liang <gcliang@mit.edu>
2015-01-21 14:55:41 -0500

    Wall Follow

--- src/TestWallFollowing.java
+++ src/TestWallFollowing.java
@@ -105,7 +105,7 @@ public class TestWallFollowing {
                     System.out.println("Too Close: " + frontSep);
                     leftMotor.setSpeed(0);
                     rightMotor.setSpeed(0);
-                    leftMotor.setSpeed(.2);
+                    leftMotor.setSpeed(.25);
                     rightMotor.setSpeed(.1);
                     try {
                         Thread.sleep(80);
