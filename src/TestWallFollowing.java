773a8aac2b1bc3df80d663d7a59ba5d3f2c1866b
George Liang <gcliang@mit.edu>
2015-01-20 23:32:46 -0500

    Single IR

--- src/TestWallFollowing.java
+++ src/TestWallFollowing.java
@@ -33,9 +33,9 @@ public class TestWallFollowing {
         long begin = System.currentTimeMillis();
         boolean log = true;
         double bias = 0.15;
-        double p = 0.35; // .012;
+        double p = 0.45; // .012;
         double i = 0; // 0.005; // 0.0005;
-        double d = 0; // 0.05; // 0.03;
+        double d = 0.05; // 0.03;
         double integral = 0;
         double derivative = 0;
         double frontSep = forwardSensor.distanceToObject();
