01016c9b1ead441260ecef13a48c33713007abcc
George Liang <gcliang@mit.edu>
2015-01-21 16:34:45 -0500

    Wall Follow

--- src/TestWallFollowing.java
+++ src/TestWallFollowing.java
@@ -37,7 +37,7 @@ public class TestWallFollowing {
         double bias = 0.2;
         double p = 0.15; // 0.15;
         double i = 0.005; // 0.01;
-        double d = 0.7; // 0.7
+        double d = 0.3; // 0.7
         double integral = 0;
         double derivative = 0;
         double sideSep = sideSensor.distanceToObject();
