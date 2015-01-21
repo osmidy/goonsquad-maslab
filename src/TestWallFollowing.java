7d9f1b2ac75eb6f125c2b474cdb7f05489c009fb
George Liang <gcliang@mit.edu>
2015-01-21 14:40:55 -0500

    Wall Follow

--- src/TestWallFollowing.java
+++ src/TestWallFollowing.java
@@ -37,7 +37,7 @@ public class TestWallFollowing {
         double bias = 0.2;
         double p = 0.15; // .012;
         double i = 0; // 0.005; // 0.0005;
-        double d = 1; // 0.03;
+        double d = 0.7; // 0.03;
         double integral = 0;
         double derivative = 0;
         double sideSep = sideSensor.distanceToObject();
@@ -131,7 +131,7 @@ public class TestWallFollowing {
                     System.out.println("Integral: " + integral + "Derivative: "
                             + derivative + "Power: " + power);
                     try {
-                        Thread.sleep(300);
+                        Thread.sleep(150);
                     } catch (InterruptedException e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
