48cb99dd46722cfef2a69c391dc45920f016f797
George Liang <gcliang@mit.edu>
2015-01-21 00:58:04 -0500

    Single IR

--- src/TestWallFollowing.java
+++ src/TestWallFollowing.java
@@ -35,7 +35,7 @@ public class TestWallFollowing {
         long begin = System.currentTimeMillis();
         boolean log = true;
         double bias = 0.15;
-        double p = 0.35; // .012;
+        double p = 0.3; // .012;
         double i = 0; // 0.005; // 0.0005;
         double d = .25; // 0.03;
         double integral = 0;
