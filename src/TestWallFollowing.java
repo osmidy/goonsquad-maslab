cd205e464947f169c854096cbc59c07bc0c24eb5
George Liang <gcliang@mit.edu>
2015-01-22 16:46:54 -0500

    Robust WallFollow

--- src/TestWallFollowing.java
+++ src/TestWallFollowing.java
@@ -64,153 +64,133 @@ public class TestWallFollowing {
                 break mainLoop;
             }
 
+            long end = System.currentTimeMillis();
+            frontSep = frontSensor.distanceToObject();
+            sideSep = sideSensor.distanceToObject();
+            double diff = sideSep - desired;
+            double deltaT = .001 * (end - begin);
+            integral += diff * deltaT;
+            derivative = diff - prevDiff;
 
-            // sideSepWindow[sideSepCounter] = forwardSensor
-            // .distanceToObject();
-            // rearSepWindow[rearSepCounter] = rearSensor.distanceToObject();
-            // sideSepCounter++;
-            // rearSepCounter++;
-            // //else {
-            // sideSepData = sideSepWindow;
-            // rearSepData = rearSepWindow;
-            // Arrays.sort(sideSepData);
-            // Arrays.sort(rearSepData);
-            // sideSep = sideSepData[10];
-            // rearSep = rearSepData[10];
-            //
-            // if (sideSepCounter > 19){
-            // sideSepCounter = 0;
-            // rearSepCounter = 0;
-
-            {
-
-                long end = System.currentTimeMillis();
-                frontSep = frontSensor.distanceToObject();
-                sideSep = sideSensor.distanceToObject();
-                double diff = sideSep - desired;
-                double deltaT = .001 * (end - begin);
-                integral += diff * deltaT;
-                derivative = diff - prevDiff;
-
-                if (integral > 500) {
-                    integral = 500;
-                }
-                
-                double power = p * diff + i * integral + d * derivative;
-                bias = 0.2;
-                leftMotor.setSpeed(bias - power);
-                rightMotor.setSpeed(bias + power);
-                begin = end;
-                prevDiff = diff;
-                
-              //left turns
-//            	if (sideSep > 0.6) {
-//            		leftMotor.setSpeed(0.05);
-//            		rightMotor.setSpeed(0.2);
-//            		try {
-//                    Thread.sleep(30);
-//                } catch (InterruptedException e) {
-//                    // TODO Auto-generated catch block
-//                    e.printStackTrace();
-//                }
-//            	}
-                
-              //approach right turns smoothly
-                if (frontSep < 0.4 && frontSep > 0.2){
-//                	double a = -1/4000;
-//                	double b = 1/50;
-//                	double c = -3/10;
-//                	double newBias = a*frontSep*frontSep + b*frontSep + c;
-                	double newBias = (0.2/0.2) * (frontSep - 0.2);
-                	leftMotor.setSpeed(newBias + 0.05);
-                	rightMotor.setSpeed(newBias - 0.05);
-                  try {
-                  Thread.sleep(30);
-              } catch (InterruptedException e) {
-                  // TODO Auto-generated catch block
-                  e.printStackTrace();
-              }
-                }
-                if (frontSep <= 0.2){
-                	leftMotor.setSpeed(0.1);
-                	rightMotor.setSpeed(-0.1);
-              try {
-              Thread.sleep(80);
-          } catch (InterruptedException e) {
-              // TODO Auto-generated catch block
-              e.printStackTrace();
-          }       
-                }
-                
-                //Approach turns in steps
-//                if (frontSep<0.4 && frontSep>0.2){
-//                	leftMotor.setSpeed(0.23);
-//                	rightMotor.setSpeed(0.1);
-//                  try {
-//                  Thread.sleep(80);
-//              } catch (InterruptedException e) {
-//                  // TODO Auto-generated catch block
-//                  e.printStackTrace();
-//              }
-//                	
-//                }
-//                if (frontSep <= 0.2){
-//                	leftMotor.setSpeed(0.1);
-//                	rightMotor.setSpeed(-0.1);
-//                  try {
-//                  Thread.sleep(80);
-//              } catch (InterruptedException e) {
-//                  // TODO Auto-generated catch block
-//                  e.printStackTrace();
-//              }
-//                }
-//                //only works with wall follow
-//                if (frontSep < 0.2) {
-//                    System.out.println("Too Close: " + frontSep);
-//                    leftMotor.setSpeed(0);
-//                    rightMotor.setSpeed(0);
-//                    leftMotor.setSpeed(0.1); //.23 // .25
-//                    rightMotor.setSpeed(-0.1); // .1
-//                    try {
-//                        Thread.sleep(80);
-//                    } catch (InterruptedException e) {
-//                        // TODO Auto-generated catch block
-//                        e.printStackTrace();
-//                    }
-//                }
-//
-//                try {
-//                    Thread.sleep(33);
-//                } catch (InterruptedException e) {
-//                    // TODO Auto-generated catch block
-//                    e.printStackTrace();
-//                }
-
-                if (log == true) {
-                    System.out.println("Front; " + frontSep);
-                    System.out.println("Side: " + sideSep);
-                    System.out.println("Left: " + leftMotor.getSpeed()
-                            + " Right: " + rightMotor.getSpeed());
-                    System.out.println("Diff: " + p * diff + "Integral: " + i * integral + "Derivative: "
-                            + d * derivative + "Power: " + power);
-                    try {
-                        Thread.sleep(150);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-
-//                     long fin = System.currentTimeMillis();
-//                     if ((fin - current) >= 5000) {
-//                     leftMotor.setSpeed(0);
-//                     rightMotor.setSpeed(0);
-//                     System.out.println("Time.");
-//                     break mainLoop;
-//                     }
-//                    
-                }
-                // System.out.println("Fin.");
+            if (integral > 500) {
+                integral = 500;
             }
+
+            double power = p * diff + i * integral + d * derivative;
+            bias = 0.2;
+            leftMotor.setSpeed(bias - power);
+            rightMotor.setSpeed(bias + power);
+            begin = end;
+            prevDiff = diff;
+
+            // left turns
+            if (sideSep > 0.6) {
+                leftMotor.setSpeed(0.07);
+                rightMotor.setSpeed(0.2);
+                try {
+                    Thread.sleep(30);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            // approach right turns smoothly
+            if (frontSep < 0.4 && frontSep > 0.2) {
+                // double a = -1/4000;
+                // double b = 1/50;
+                // double c = -3/10;
+                // double newBias = a*frontSep*frontSep + b*frontSep + c;
+                double newBias = (frontSep - 0.2);
+                leftMotor.setSpeed(newBias + 0.05);
+                rightMotor.setSpeed(newBias - 0.05);
+                try {
+                    Thread.sleep(30);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            if (frontSep <= 0.2) {
+                leftMotor.setSpeed(0.1);
+                rightMotor.setSpeed(-0.1);
+                try {
+                    Thread.sleep(80);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            // Approach turns in steps
+            // if (frontSep<0.4 && frontSep>0.2){
+            // leftMotor.setSpeed(0.23);
+            // rightMotor.setSpeed(0.1);
+            // try {
+            // Thread.sleep(80);
+            // } catch (InterruptedException e) {
+            // // TODO Auto-generated catch block
+            // e.printStackTrace();
+            // }
+            //
+            // }
+            // if (frontSep <= 0.2){
+            // leftMotor.setSpeed(0.1);
+            // rightMotor.setSpeed(-0.1);
+            // try {
+            // Thread.sleep(80);
+            // } catch (InterruptedException e) {
+            // // TODO Auto-generated catch block
+            // e.printStackTrace();
+            // }
+            // }
+            // //only works with wall follow
+            // if (frontSep < 0.2) {
+            // System.out.println("Too Close: " + frontSep);
+            // leftMotor.setSpeed(0);
+            // rightMotor.setSpeed(0);
+            // leftMotor.setSpeed(0.1); //.23 // .25
+            // rightMotor.setSpeed(-0.1); // .1
+            // try {
+            // Thread.sleep(80);
+            // } catch (InterruptedException e) {
+            // // TODO Auto-generated catch block
+            // e.printStackTrace();
+            // }
+            // }
+            //
+            // try {
+            // Thread.sleep(33);
+            // } catch (InterruptedException e) {
+            // // TODO Auto-generated catch block
+            // e.printStackTrace();
+            // }
+
+            if (log == true) {
+                System.out.println("Front; " + frontSep);
+                System.out.println("Side: " + sideSep);
+                System.out.println("Left: " + leftMotor.getSpeed() + " Right: "
+                        + rightMotor.getSpeed());
+                System.out.println("Diff: " + p * diff + "Integral: " + i
+                        * integral + "Derivative: " + d * derivative
+                        + "Power: " + power);
+                try {
+                    Thread.sleep(150);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                // long fin = System.currentTimeMillis();
+                // if ((fin - current) >= 5000) {
+                // leftMotor.setSpeed(0);
+                // rightMotor.setSpeed(0);
+                // System.out.println("Time.");
+                // break mainLoop;
+                // }
+                //
+            }
+            // System.out.println("Fin.");
         }
     }
 }
