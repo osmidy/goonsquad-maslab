14592378c5e076a422f5923c2b3fe322d5ad9e8e
George Liang <gcliang@mit.edu>
2015-01-23 00:34:06 -0500

    Robust WallFollow

--- src/ExperimentalWF.java
+++ src/ExperimentalWF.java
@@ -92,8 +92,8 @@ public class ExperimentalWF {
                 System.out.println("(0,1)" + "(" + sideSep + "," + diagonalSep
                         + ")");
                 diagonalSep = diagonalSensor.distanceToObject();
-                leftMotor.setSpeed(0.08);
-                rightMotor.setSpeed(0.1);
+                leftMotor.setSpeed(0.1);
+                rightMotor.setSpeed(0.05);
                 // leftMotor.setSpeed(0.25); // .23 // .25
                 // rightMotor.setSpeed(0.1); // .1
                 try {
