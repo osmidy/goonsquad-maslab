f569783732d6f882b958d06061506e31e14cad91
George Liang <gcliang@mit.edu>
2015-01-24 16:52:10 -0500

    Prototype 1

--- src/TestWallFollowing.java
+++ src/TestWallFollowing.java
@@ -74,8 +74,8 @@ public class TestWallFollowing {
 	         while (diagonalSep < 0.15) {
 	                System.out.println("Too Close: " + diagonalSep);
 	                diagonalSep = diagonalSensor.distanceToObject();
-	                leftMotor.setSpeed(0.1);
-	                rightMotor.setSpeed(-0.1);
+	                leftMotor.setSpeed(0.2);
+	                rightMotor.setSpeed(-0.2);
 //	              leftMotor.setSpeed(0.25); // .23 // .25
 //	              rightMotor.setSpeed(0.1); // .1
 	              try {
@@ -179,7 +179,6 @@ public class TestWallFollowing {
 			}
 
 			if (log == true) {
-				System.out.println("Front: " + frontSep);
 				System.out.println("Side: " + sideSep + "Diagonal: "
 						+ diagonalSep);
 				System.out.println("Left: " + leftMotor.getSpeed() + " Right: "
