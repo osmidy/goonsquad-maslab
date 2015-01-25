c65e54491ba72d8b7efe9a08e2afe1ed40f7ac5e
Chicken <medinaa@mit.edu>
2015-01-24 19:09:42 -0500

    wall follower

--- src/ExperimentalWF.java
+++ src/ExperimentalWF.java
@@ -37,7 +37,7 @@ public class ExperimentalWF {
         long begin = System.currentTimeMillis();
         boolean log = true;
         double bias = 0.2;
-        double p = 0.5;
+        double p = 0.4; //0.5;
         double i = 0.005; // .005 // 0.01;
         double d = 0.5;// .3 // 0.7
         double integral = 0;
