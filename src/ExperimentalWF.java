34b97148c1d67193686ae8ff4a7b3b62dfede47b
Chicken <medinaa@mit.edu>
2015-01-24 20:18:38 -0500

    wall follower

--- src/ExperimentalWF.java
+++ src/ExperimentalWF.java
@@ -161,11 +161,11 @@ public class ExperimentalWF {
                 }
 
                 double power = p * diff + i * integral + d * derivative;
-                bias = 0.15;
-                if (diagonalSep < 0.4){
-                	bias = 0.1;
-                	power = power - ((diagonalSep-0.4)/8);
-                }
+                bias = 0.2; //0.15;
+//                if (diagonalSep < 0.4){
+//                	bias = 0.1;
+//                	power = power - ((diagonalSep-0.4)/8);
+//                }
                 leftMotor.setSpeed(bias - power);
                 rightMotor.setSpeed(bias + power);
                 begin = end;
