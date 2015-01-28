dea5a55ee670fcb77e92c9c7bd66bbee8971febc
Olivier Midy <osmidy@mit.edu>
2015-01-28 01:43:23 -0500

    Testing wall follow

--- src/ExperimentalWF.java
+++ src/ExperimentalWF.java
@@ -39,7 +39,7 @@ public class ExperimentalWF {
         double bias = 0.15;
         double p = 0.5;
         double i = 0.005; // .005 // 0.01;
-        double d = .8; // 0.08 //0.5;// .3 // 0.7
+        double d = .08; // 0.08 //0.5;// .3 // 0.7
         double integral = 0;
         double derivative = 0;
         double sideSep = sideSensor.distanceToObject();
