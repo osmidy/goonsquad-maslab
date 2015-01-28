227c9416472cf2a7d4a1ad5627b2802e20d2d1b9
Olivier Midy <osmidy@mit.edu>
2015-01-27 19:51:26 -0500

    TEST COMMIT

--- src/imageprocessing_ObjectFinder.cpp
+++ src/imageprocessing_ObjectFinder.cpp
@@ -27,33 +27,33 @@ JNIEXPORT jint JNICALL Java_imageprocessing_ObjectFinder_checkCube
 	double minimumArea = 142538 *  pow(distance, -1.839); // 610099 * pow(distance, -1.892);
 	// Check surrounding pixels; assume shape is roughly square
 	double radius = .5 * pow(minimumArea, .5);
-//	if (x < radius || x > width - radius) {
-//		threshold = 0.35;
-//	} else {
-//		threshold = 0.7;
-//	}
-//	if (y < radius || y > height - radius) {
-//		threshold = 0.35;
-//	} else {
-//		threshold = 0.7;
-//	}
-//
-//	int greenCount = 0;
-//	int redCount = 0;
-//	for (int i = y - radius; i >= 0 && i < y + radius; i++) {
-//		for (int j = x - radius; j >= 0 && j < x + radius; j++) {
-//			// Will change for specific color cubes; for now take red and green
-//			int val0 = image->data[channels * (image->cols * i + j) + 0];
-//			int val1 = image->data[channels * (image->cols * i + j) + 1];
-//			int val2 = image->data[channels * (image->cols * i + j) + 2];
-//			// if green or red
-//			if ((val0 == 0) && (val1 == 255) && (val2 == 0)) {
-//				greenCount++;
-//			} else if ((val0 == 0) && (val1 == 0) && (val2 == 255)) {
-//				redCount++;
-//			}
-//		}
-//	}
+	if (x < radius || x > width - radius) {
+		threshold = 0.35;
+	} else {
+		threshold = 0.7;
+	}
+	if (y < radius || y > height - radius) {
+		threshold = 0.35;
+	} else {
+		threshold = 0.7;
+	}
+
+	int greenCount = 0;
+	int redCount = 0;
+	for (int i = y - radius; i >= 0 && i < y + radius; i++) {
+		for (int j = x - radius; j >= 0 && j < x + radius; j++) {
+			// Will change for specific color cubes; for now take red and green
+			int val0 = image->data[channels * (image->cols * i + j) + 0];
+			int val1 = image->data[channels * (image->cols * i + j) + 1];
+			int val2 = image->data[channels * (image->cols * i + j) + 2];
+			// if green or red
+			if ((val0 == 0) && (val1 == 255) && (val2 == 0)) {
+				greenCount++;
+			} else if ((val0 == 0) && (val1 == 0) && (val2 == 255)) {
+				redCount++;
+			}
+		}
+	}
 //	if ((redCount > 0)
 //			&& ((minimumArea) > redCount
 //					&& redCount >= (minimumArea * threshold))) {
