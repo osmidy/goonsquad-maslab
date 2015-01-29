7549bceaddc3af4ff1b3c931ad30675abdcf17ea
George Liang <gcliang@mit.edu>
2015-01-29 13:28:33 -0500

    TestServo file

---
+++ src/TestServo.java
@@ -0,0 +1,27 @@
+import robotparts.Servo;
+import sensorIO.Shield;
+
+
+public class TestServo {
+    static int bus = 6; // Always for shield on Edison
+    static Shield shield = new Shield(bus);
+    static Servo servo = new Servo(0, shield);
+    public static void main(String[] args) {
+        while (true) {
+            servo.setPosition(.2);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            servo.setPosition(.8);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
