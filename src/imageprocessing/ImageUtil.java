6c57a96bd38356097875f4a5539afc5f585c04c1
Olivier Midy <osmidy@mit.edu>
2015-01-30 10:15:55 -0500

    Final tests

--- src/imageprocessing/ImageUtil.java
+++ src/imageprocessing/ImageUtil.java
@@ -185,7 +185,7 @@ public class ImageUtil {
                 y = pixel[1];
             }
         }
-        heading = getHeading(x + 22.5); // Camera offset from center
+        heading = getHeading(x + 22); // Camera offset from center
         ImageCube cube = new ImageCube(x, y, closestDistance, heading);
         return cube;
     }
